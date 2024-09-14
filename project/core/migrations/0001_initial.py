f9653375bf016aeaa135ca026badc51f9bac1fcc
Dan Sahagian <45240763+dansahagian@users.noreply.github.com>
2024-09-14 15:33:30 -0700

    Run the formatter

--- project/core/migrations/0001_initial.py
+++ project/core/migrations/0001_initial.py
@@ -21,9 +21,7 @@ class Migration(migrations.Migration):
                 ("password", models.CharField(max_length=128, verbose_name="password")),
                 (
                     "last_login",
-                    models.DateTimeField(
-                        blank=True, null=True, verbose_name="last login"
-                    ),
+                    models.DateTimeField(blank=True, null=True, verbose_name="last login"),
                 ),
                 (
                     "is_superuser",
@@ -35,21 +33,15 @@ class Migration(migrations.Migration):
                 ),
                 (
                     "first_name",
-                    models.CharField(
-                        blank=True, max_length=150, verbose_name="first name"
-                    ),
+                    models.CharField(blank=True, max_length=150, verbose_name="first name"),
                 ),
                 (
                     "last_name",
-                    models.CharField(
-                        blank=True, max_length=150, verbose_name="last name"
-                    ),
+                    models.CharField(blank=True, max_length=150, verbose_name="last name"),
                 ),
                 (
                     "email",
-                    models.EmailField(
-                        blank=True, max_length=254, verbose_name="email address"
-                    ),
+                    models.EmailField(blank=True, max_length=254, verbose_name="email address"),
                 ),
                 (
                     "is_staff",
@@ -86,9 +78,7 @@ class Migration(migrations.Migration):
                 ("updated_at", models.DateTimeField(auto_now=True)),
                 (
                     "username",
-                    models.EmailField(
-                        max_length=254, unique=True, verbose_name="Email Address"
-                    ),
+                    models.EmailField(max_length=254, unique=True, verbose_name="Email Address"),
                 ),
                 (
                     "groups",
